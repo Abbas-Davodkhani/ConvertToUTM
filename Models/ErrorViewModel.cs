c0322dfcde29f7b94b165584c443ea41ff028f48
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2023-01-21 19:11:44 +0330

    convert

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Convert_LatLon_to_UTM.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
